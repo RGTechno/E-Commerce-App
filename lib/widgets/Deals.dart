508e4fb1306760da64dcb5781b9ed627edc03bdb
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2020-12-18 16:38:58 +0530

    grid ui

--- lib/widgets/Deals.dart
+++ lib/widgets/Deals.dart
@@ -4,34 +4,47 @@ import 'package:flutter/material.dart';
 class Deals extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 500,
-      child: GridView.count(
-        physics: ScrollPhysics(),
-        crossAxisCount: 2,
-        childAspectRatio: 0.75,
-        children: [
-          DealItem(
-            color: Colors.lightGreen,
-            title: "OnePlus Nord",
-            price: 29000,
-          ),
-          DealItem(
-            color: Colors.lightGreen,
-            title: "OnePlus Nord",
-            price: 29000,
-          ),
-          DealItem(
-            color: Colors.lightGreen,
-            title: "OnePlus Nord",
-            price: 29000,
-          ),
-          DealItem(
-            color: Colors.lightGreen,
-            title: "OnePlus Nord",
-            price: 29000,
-          ),
-        ],
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 17),
+        decoration: BoxDecoration(
+          color: Colors.grey.withOpacity(0.1),
+          borderRadius: BorderRadius.circular(20)
+        ),
+        constraints: BoxConstraints(
+          maxWidth: double.infinity,
+          minHeight: 200,
+          maxHeight: 400,
+        ),
+        child: GridView.count(
+          physics: ScrollPhysics(),
+          crossAxisCount: 2,
+          crossAxisSpacing: 2,
+          mainAxisSpacing: 0.6,
+          children: [
+            DealItem(
+              color: Colors.lightGreen,
+              title: "OnePlus Nord",
+              price: 29000,
+            ),
+            DealItem(
+              color: Colors.lightGreen,
+              title: "OnePlus Nord",
+              price: 29000,
+            ),
+            DealItem(
+              color: Colors.lightGreen,
+              title: "OnePlus Nord",
+              price: 29000,
+            ),
+            DealItem(
+              color: Colors.lightGreen,
+              title: "OnePlus Nord",
+              price: 29000,
+            ),
+          ],
+        ),
       ),
     );
   }
