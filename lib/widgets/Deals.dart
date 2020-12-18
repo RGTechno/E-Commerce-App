b5efb378c53364a87e364d0ac1469b5ad0157eae
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2020-12-18 16:51:30 +0530

    deal done

--- lib/widgets/Deals.dart
+++ lib/widgets/Deals.dart
@@ -1,5 +1,6 @@
 import 'package:e_commerce_sample/widgets/Deal_Item.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Deals extends StatelessWidget {
   @override
@@ -9,39 +10,55 @@ class Deals extends StatelessWidget {
       child: Container(
         padding: const EdgeInsets.symmetric(vertical: 17),
         decoration: BoxDecoration(
-          color: Colors.grey.withOpacity(0.1),
-          borderRadius: BorderRadius.circular(20)
-        ),
-        constraints: BoxConstraints(
-          maxWidth: double.infinity,
-          minHeight: 200,
-          maxHeight: 400,
-        ),
-        child: GridView.count(
-          physics: ScrollPhysics(),
-          crossAxisCount: 2,
-          crossAxisSpacing: 2,
-          mainAxisSpacing: 0.6,
+            color: Colors.grey.withOpacity(0.1),
+            borderRadius: BorderRadius.circular(20)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            DealItem(
-              color: Colors.lightGreen,
-              title: "OnePlus Nord",
-              price: 29000,
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Text(
+                "Deals",
+                style: GoogleFonts.gafata(
+                  fontSize: 27,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
-            DealItem(
-              color: Colors.deepOrangeAccent,
-              title: "Apple iPhone 11",
-              price: 69000,
-            ),
-            DealItem(
-              color: Colors.teal,
-              title: "HP Pavilion",
-              price: 79000,
-            ),
-            DealItem(
-              color: Colors.pinkAccent.shade700,
-              title: "Harry Potter Kindle Pack",
-              price: 290,
+            Container(
+              constraints: BoxConstraints(
+                maxWidth: double.infinity,
+                minHeight: 200,
+                maxHeight: 400,
+              ),
+              child: GridView.count(
+                physics: ScrollPhysics(),
+                crossAxisCount: 2,
+                crossAxisSpacing: 2,
+                mainAxisSpacing: 0.6,
+                children: [
+                  DealItem(
+                    color: Colors.lightGreen,
+                    title: "OnePlus Nord",
+                    price: 29000,
+                  ),
+                  DealItem(
+                    color: Colors.deepOrangeAccent,
+                    title: "Apple iPhone 11",
+                    price: 69000,
+                  ),
+                  DealItem(
+                    color: Colors.teal,
+                    title: "HP Pavilion",
+                    price: 79000,
+                  ),
+                  DealItem(
+                    color: Colors.pinkAccent.shade700,
+                    title: "Harry Potter Kindle Pack",
+                    price: 290,
+                  ),
+                ],
+              ),
             ),
           ],
         ),
