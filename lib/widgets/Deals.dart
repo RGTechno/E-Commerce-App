39c32ea8b41b5624177f0735080337c32a2b1ce3
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2020-12-18 16:06:22 +0530

    deal widget

---
+++ lib/widgets/Deals.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Deals extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: GridView.count(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
